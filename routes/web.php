8e6db340a3fab3231e23288d2afb3bbe9d01cdd5
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-08-08 16:33:10 +0600

    routes updated

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
-
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+Route::get('/',function (){
+    return redirect('/admin');
+});
 
 require __DIR__.'/auth.php';
