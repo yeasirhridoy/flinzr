033c2d6d4f64c7295cbc7763a4842930f12ed526
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-08-17 18:08:02 +0600

    updated

--- app/Filament/Pages/Auth/EditProfile.php
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -14,6 +14,7 @@ class EditProfile extends \Filament\Pages\Auth\EditProfile
             $this->getNameFormComponent(),
             $this->getEmailFormComponent(),
             $this->getPasswordFormComponent(),
+            $this->getPasswordConfirmationFormComponent(),
         ]);
     }
 }
