b860bbc39361ad74f860f84594951a91e10412ea
Hridoy <yeasirarafat321@gmail.com>
2024-08-28 22:44:37 +0600

    response updated

---
+++ app/Http/Middleware/ResponseMiddleware.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+        if ($response->headers->get('Content-Type') === 'application/json' && $response->isSuccessful()) {
+            $response->setContent(json_encode([
+                'success' => true,
+                'message' => 'Success',
+                'data' => json_decode($response->getContent())
+            ]));
+        } elseif ($response->headers->get('Content-Type') === 'application/json' && !$response->isSuccessful()) {
+            $response->setContent(json_encode([
+                'success' => false,
+                'message' => 'Error',
+                'data' => json_decode($response->getContent())
+            ]));
+        }
+        return $response;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -9,29 +9,31 @@ use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/recover-password', [AuthController::class, 'recoverPassword']);
-Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+Route::middleware(\App\Http\Middleware\ResponseMiddleware::class)->group(function (){
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/recover-password', [AuthController::class, 'recoverPassword']);
+    Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-Route::get('collections', [CollectionController::class, 'index']);
-Route::get('categories', [CategoryController::class, 'index']);
-Route::get('tags', [TagController::class, 'index']);
-Route::get('colors', [ColorController::class, 'index']);
+    Route::get('collections', [CollectionController::class, 'index']);
+    Route::get('categories', [CategoryController::class, 'index']);
+    Route::get('tags', [TagController::class, 'index']);
+    Route::get('colors', [ColorController::class, 'index']);
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/user', [AuthController::class, 'user']);
-    Route::put('/user', [AuthController::class, 'updateUser']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/otp', [AuthController::class, 'otp']);
-    Route::post('/verify-email', [AuthController::class, 'verifyEmail']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/user', [AuthController::class, 'user']);
+        Route::put('/user', [AuthController::class, 'updateUser']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/otp', [AuthController::class, 'otp']);
+        Route::post('/verify-email', [AuthController::class, 'verifyEmail']);
 
-    Route::post('collections', [CollectionController::class, 'store']);
+        Route::post('collections', [CollectionController::class, 'store']);
 
-    Route::get('favorites', [FavoriteController::class, 'index']);
-    Route::post('favorites', [FavoriteController::class, 'store']);
+        Route::get('favorites', [FavoriteController::class, 'index']);
+        Route::post('favorites', [FavoriteController::class, 'store']);
 
-    Route::get('artists', [UserController::class, 'artists']);
-    Route::post('artist-request', [UserController::class, 'artistRequest']);
-    Route::get('artist-request', [UserController::class, 'myArtistRequest']);
+        Route::get('artists', [UserController::class, 'artists']);
+        Route::post('artist-request', [UserController::class, 'artistRequest']);
+        Route::get('artist-request', [UserController::class, 'myArtistRequest']);
+    });
 });
