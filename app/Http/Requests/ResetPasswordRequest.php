819c33354d6b030d5ea39e6548a5d3c94c65665a
Hridoy <yeasirarafat321@gmail.com>
2024-09-13 20:24:15 +0600

    response updated

--- app/Http/Requests/ResetPasswordRequest.php
+++ app/Http/Requests/ResetPasswordRequest.php
@@ -24,7 +24,6 @@ class ResetPasswordRequest extends FormRequest
     {
         return [
             'email' => 'required|email|exists:users,email',
-            'otp' => 'required|digits:6',
             'password' => 'required|string|min:6|confirmed',
         ];
     }
