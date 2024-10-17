71abd1abdeef604e94feb4e38d85e9df8e63d4a0
Hridoy <yeasirarafat321@gmail.com>
2024-10-17 21:19:31 +0600

    updated

--- app/Http/Resources/MinimumUserResource.php
+++ app/Http/Resources/MinimumUserResource.php
@@ -19,7 +19,7 @@ class MinimumUserResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'username' => $this->username,
-            'image' => $this->image ? Storage::url($this->image) : null,
+            'image' => $this->image,
             'followers_count' => $this->followers_count ?? $this->followers()->count(),
             'followings_count' => $this->followings_count ?? $this->followings()->count(),
             'is_following' => $this->when(auth('sanctum')->check(), $this->is_following ?? $this->followers->contains('id', auth('sanctum')->id())),
