626cb5fb69c553c0f604c31b4f3b00df5168e78a
Yeasir Arafat <yeasirarafat321@gmail.com>
2024-11-13 22:07:47 +0600

    updated

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Jobs\AddCoinsToInfluencer;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+Schedule::job(new AddCoinsToInfluencer)->dailyAt('00:00');
