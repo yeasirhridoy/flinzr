41cc192145893d07b49bd1465f27ddafffddcd42
Hridoy <yeasirarafat321@gmail.com>
2024-12-07 20:50:53 +0600

    updated

--- routes/console.php
+++ routes/console.php
@@ -5,7 +5,4 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 Schedule::job(new AddCoinsToInfluencer)->dailyAt('00:00');
