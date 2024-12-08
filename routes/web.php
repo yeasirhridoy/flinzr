542606d77def8557eee28497037dead4df31aa89
Hridoy <yeasirarafat321@gmail.com>
2024-12-08 22:09:07 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,12 @@ Route::get('/',function (){
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('logs',function (){
+    return response(file_get_contents(storage_path('logs/laravel.log')))->header('Content-Type', 'text/plain');
+});
+
+Route::get('clear-log',function (){
+    file_put_contents(storage_path('logs/laravel.log'),'');
+    return redirect('logs');
+});
