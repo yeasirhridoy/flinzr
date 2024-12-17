6b7afcb2bc68cb355b7ded7b23e7cbc3f5499d2a
nazia <naziatabassum21795@gmail.com>
2024-12-18 01:09:58 +0600

    attachments

--- app/Livewire/SendMessageForm.php
+++ app/Livewire/SendMessageForm.php
@@ -7,6 +7,7 @@ use Filament\Forms\Components\TextInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
+use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
 
 class SendMessageForm extends Component implements HasForms
@@ -42,6 +43,11 @@ class SendMessageForm extends Component implements HasForms
     public function sendMessage(): void
     {
         $data = $this->form->getState();
+
+        if (!empty($data['attachments'])) {
+            $data['attachments'] = Storage::url($data['attachments']);
+        }
+
         $data['sender'] = 'admin';
         $this->dispatch('messageSent', $data);
         $this->form->fill();
